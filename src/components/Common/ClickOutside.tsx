6a5d2f62aeff8fdf7322054ecf79fb5c1517478f
SanjulaD <desanjula99@gmail.com>
2024-07-07 00:24:55 +0530

    feat: prettier changes

--- src/components/Common/ClickOutside.tsx
+++ src/components/Common/ClickOutside.tsx
@@ -1,53 +1,56 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect } from 'react';
 
 interface Props {
-    children: React.ReactNode;
-    exceptionRef?: React.RefObject<HTMLElement>;
-    onClick: () => void;
-    className?: string;
+  children: React.ReactNode;
+  exceptionRef?: React.RefObject<HTMLElement>;
+  onClick: () => void;
+  className?: string;
 }
 
 const ClickOutside: React.FC<Props> = ({
-    children,
-    exceptionRef,
-    onClick,
-    className,
+  children,
+  exceptionRef,
+  onClick,
+  className,
 }) => {
-    const wrapperRef = useRef<HTMLDivElement>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        const handleClickListener = (event: MouseEvent) => {
-            let clickedInside = false;
+  useEffect(() => {
+    const handleClickListener = (event: MouseEvent) => {
+      let clickedInside = false;
 
-            if (wrapperRef.current && wrapperRef.current.contains(event.target as Node)) {
-                clickedInside = true;
-            }
+      if (
+        wrapperRef.current &&
+        wrapperRef.current.contains(event.target as Node)
+      ) {
+        clickedInside = true;
+      }
 
-            if (exceptionRef && exceptionRef.current) {
-                if (exceptionRef.current === event.target) {
-                    clickedInside = true;
-                } else if (exceptionRef.current.contains(event.target as Node)) {
-                    clickedInside = true;
-                }
-            }
+      if (exceptionRef && exceptionRef.current) {
+        if (exceptionRef.current === event.target) {
+          clickedInside = true;
+        } else if (exceptionRef.current.contains(event.target as Node)) {
+          clickedInside = true;
+        }
+      }
 
-            if (!clickedInside) {
-                onClick();
-            }
-        };
+      if (!clickedInside) {
+        onClick();
+      }
+    };
 
-        document.addEventListener("mousedown", handleClickListener);
+    document.addEventListener('mousedown', handleClickListener);
 
-        return () => {
-            document.removeEventListener("mousedown", handleClickListener);
-        };
-    }, [exceptionRef, onClick]);
+    return () => {
+      document.removeEventListener('mousedown', handleClickListener);
+    };
+  }, [exceptionRef, onClick]);
 
-    return (
-        <div ref={wrapperRef} className={className || ""}>
-            {children}
-        </div>
-    );
+  return (
+    <div ref={wrapperRef} className={className || ''}>
+      {children}
+    </div>
+  );
 };
 
 export default ClickOutside;
